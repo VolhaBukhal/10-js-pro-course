eb50a7aadd5928fffe67c211b9572369b49f3e89
volha bukhal <o.bukhal@gmail.com>
2021-09-15 22:32:58 +0300

    add husky folder

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -4,7 +4,7 @@ const { smartSearch, smartIndex } = require('tms-lesson32');
 console.log(smartSearch(['Smith', 'John'], 'John'));
 console.log(smartIndex('Smith', 'th'));
 
-function printArr(arr) {
+function printArr(arr){
   console.log(arr);
 }
 printArr(['Smith', 'John']);
